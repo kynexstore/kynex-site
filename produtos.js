c3273aed140638550466a8796c5d5fd85c86c8a1
kynexstore <kynex@outlook.com.br>
2026-07-19 15:10:53 -0300

    Create produtos.js

---
+++ produtos.js
@@ -0,0 +1,25 @@
+const produtos = [
+    {
+        nome: "Teclado Magnético 60%",
+        descricao: "Teclado Hall Effect para jogos competitivos.",
+        preco: "R$ 299,90",
+        imagem: "https://via.placeholder.com/300",
+        link: "#"
+    },
+
+    {
+        nome: "Mouse Gamer Ultra Leve",
+        descricao: "Alta precisão para FPS.",
+        preco: "R$ 159,90",
+        imagem: "https://via.placeholder.com/300",
+        link: "#"
+    },
+
+    {
+        nome: "Mousepad Premium",
+        descricao: "Mais controle e conforto no setup.",
+        preco: "R$ 89,90",
+        imagem: "https://via.placeholder.com/300",
+        link: "#"
+    }
+];
