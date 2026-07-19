666d6d1ac56edb81fc34246ad50dc73a6825ee69
kynexstore <kynex@outlook.com.br>
2026-07-19 15:04:10 -0300

    Add button click alert for product offer

---
+++ script.js
@@ -0,0 +1,13 @@
+// KYNEX - Interações do site
+
+const botoes = document.querySelectorAll("button");
+
+botoes.forEach(botao => {
+
+    botao.addEventListener("click", () => {
+
+        alert("Em breve você será direcionado para a oferta do produto!");
+
+    });
+
+});
