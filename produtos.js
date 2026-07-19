273bc01a0769a09867a0cd3816f55e19b27e27f1
kynexstore <kynex@outlook.com.br>
2026-07-19 15:35:21 -0300

    Update produtos.js

--- produtos.js
+++ produtos.js
@@ -5,7 +5,7 @@ const produtos = [
     categoria: "Teclados",
     descricao: "Teclado magnético Hall Effect 75% para alta precisão em jogos FPS.",
     preco: "Ver oferta",
-    imagem: "https://ae01.alicdn.com/kf/S8e9f8b7a3e3b4b8e9b7f1a.jpg",
+    imagem: "https://images.unsplash.com/photo-1587829741301-dc798b83add3",
     link: "#"
 },
 
@@ -14,7 +14,7 @@ const produtos = [
     categoria: "Teclados",
     descricao: "Teclado compacto magnético 60% para setups minimalistas.",
     preco: "Ver oferta",
-    imagem: "https://ae01.alicdn.com/kf/S5e3c7d8a.jpg",
+    imagem: "https://images.unsplash.com/photo-1527814050087-3793815479db",
     link: "#"
 },
 
@@ -23,7 +23,7 @@ const produtos = [
     categoria: "Mouses",
     descricao: "Mouse leve e preciso para jogos competitivos.",
     preco: "Ver oferta",
-    imagem: "https://ae01.alicdn.com/kf/S6a9d7f1.jpg",
+    imagem: "https://images.unsplash.com/photo-1615663245857-ac93bb7c39e7",
     link: "#"
 },
 
