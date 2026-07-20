dd8fb69bfcd0eddf4d0b145e290aab41143288e2
kynexstore <kynex@outlook.com.br>
2026-07-20 10:44:36 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,56 +1,118 @@
 const lista = document.getElementById("lista-produtos");
 
-function mostrarProdutos(categoria) {
+
+function mostrarProdutos(categoria){
 
     lista.innerHTML = "";
 
-    const produtosFiltrados =
-        categoria === "Todos"
-            ? produtos
-            : produtos.filter(produto => produto.categoria === categoria);
+
+    let produtosFiltrados;
+
+
+    if(categoria === "Todos"){
+
+        produtosFiltrados = produtos;
+
+    } else {
+
+        produtosFiltrados = produtos.filter(
+            produto => produto.categoria === categoria
+        );
+
+    }
+
+
 
     produtosFiltrados.forEach(produto => {
+
 
         lista.innerHTML += `
 
         <div class="card">
 
+
             <div class="badge">
                 ${produto.badge}
             </div>
 
-            <img src="${produto.imagem}" alt="${produto.nome}">
 
-            <h3>${produto.nome}</h3>
+            <img 
+            src="${produto.imagem}" 
+            alt="${produto.nome}"
+            >
 
-            <p>${produto.descricao}</p>
+
+
+            <h3>
+                ${produto.nome}
+            </h3>
+
+
+
+            <p>
+                ${produto.descricao}
+            </p>
+
+
 
             <div class="avaliacao">
                 ⭐ ${produto.avaliacao}
             </div>
 
+
+
             <div class="vendidos">
                 ${produto.vendidos}
             </div>
+
+
 
             <div class="preco-antigo">
                 ${produto.precoAntigo}
             </div>
 
+
+
             <div class="preco">
                 ${produto.preco}
             </div>
 
-            <button onclick="window.open('${produto.link}','_blank')">
+
+
+            <button onclick="abrirOferta('${produto.link}')">
+
                 Ver oferta
+
             </button>
+
+
 
         </div>
 
         `;
 
+
     });
 
+
 }
+
+
+
+function abrirOferta(link){
+
+    if(link === "#" || link === ""){
+
+        alert("Link da oferta será adicionado em breve!");
+
+    }else{
+
+        window.open(link,"_blank");
+
+    }
+
+}
+
+
 
 mostrarProdutos("Todos");
