7ccc117558b2f91894205e1606bd2a2919d42d15
kynexstore <kynex@outlook.com.br>
2026-07-20 09:52:43 -0300

    Update produtos.js

--- produtos.js
+++ produtos.js
@@ -25,7 +25,7 @@ link:"#"
 },
 
 {
-nome:"Mousepad Premium",
+nome:"PK control 1",
 categoria:"Setup",
 descricao:"Controle e velocidade para seu setup.",
 imagem:"https://images.unsplash.com/photo-1615663245857-ac93bb7c39e7",
